c5365bec42eefbb61832d3c881d37f5d911b3028
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 16:46:04 +0900

    Add User.digest method to make *_digest(e.g. password_digest)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates :name,        presence: true, length: { maximum: 20 }
   has_secure_password
   validates :password,    presence: true, length: { minimum: 8 }
+
+  # Make digest
+  def self.digest(unencrypted)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(unencrypted, cost: cost))
+  end
 end
