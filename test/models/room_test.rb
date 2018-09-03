40894d7e12fdd7f658486d77b80e812e9e27f111
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 16:27:15 +0900

    Add room_test cases

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room = Room.new(name: "メイン部屋", description: "メインで使用する部屋")
+  end
+
+  test "should permit blank description" do
+    @room.description = "   　"
+    assert @room.valid?
+  end
+
+  test "should permit nil description" do
+    @room.description = nil
+    assert @room.valid?
+  end
+
+  test "name should be at most 30 characters" do
+    @room.description = "あいう" * 10 # 30 characters
+    assert @room.valid?
+
+    @room.description = "あいう" * 10 + "え" # 31 characters
+    assert_not @room.valid?
+  end
 end
