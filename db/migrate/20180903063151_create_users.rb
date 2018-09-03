4dbc8cc19aa10ba1507799c7343420302df27254
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 15:34:51 +0900

    Add :screen_name to CreateUsers migration

--- db/migrate/20180903063151_create_users.rb
+++ db/migrate/20180903063151_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :screen_name
       t.string :name
       t.string :password_digest
       t.string :remember_digest
