293e297e72bb35b4978a82ea35550a2213839fc0
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 22:19:30 +0900

    Add borrowed? method

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,13 @@
 class Room < ApplicationRecord
   has_many :activities, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
+
+  # A key for this room borrowed?
+  #
+  # To avoid N+1 problem, use below.
+  # Room.includes(:activities)
+  def borrowed?
+    return false if activities.empty?
+    activities.take.borrowing? 
+  end
 end
