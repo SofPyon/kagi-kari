ef81fa52b981f53efea421f135a375eefd9af052
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 17:42:03 +0900

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(title)
+    site_title = "Kagikari"
+
+    if title.blank?
+      site_title
+    else
+      title + " | " + site_title
+    end
+  end
 end
