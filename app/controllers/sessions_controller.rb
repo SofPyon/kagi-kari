4a64c6c0eca0346f4942414ba086684470899042
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 18:34:25 +0900

    Implement SessionsController.create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by_screen_name(params[:session][:screen_name])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      # redirect_to root_path
+    else
+      redirect_to new_session_path, alert: "Invalid ID/Password Combination."
+    end
+  end
 end
