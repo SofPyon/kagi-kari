b869b038001707aad8cc33ce85e13f1d2d02c4d3
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 19:05:12 +0900

    Make static_pages#index root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-
+  root 'static_pages#index'
   resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
