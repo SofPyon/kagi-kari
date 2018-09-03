2e34f8a2d394196a9f6eb8fbc2eda7b6bea63b92
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 15:49:11 +0900

    Execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903063711) do
+ActiveRecord::Schema.define(version: 20180903064125) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.integer "action", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_activities_on_room_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
