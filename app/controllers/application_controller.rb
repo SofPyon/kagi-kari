a26a1ee71e73da98f7c21f127be11c2ed13fc1e0
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 21:15:58 +0900

    Implement require_log_in method for before_action filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   layout 'main'
+
+  private
+
+    # Before actions
+
+    def require_log_in
+      unless current_user
+        flash[:danger] = "Please log in."
+        redirect_to new_session_path
+      end
+    end
 end
