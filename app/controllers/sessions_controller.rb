a9294756b4dc1ae5c32f4d67375744655d275254
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 18:50:27 +0900

    Change log in process to use sessions_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_screen_name(params[:session][:screen_name])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+      log_in user
       # redirect_to root_path
     else
       flash[:danger] = "Invalid ID/Password Combination."
