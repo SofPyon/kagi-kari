b4cf5d6f04658768492ab92c008b3d8ca29af116
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 15:37:59 +0900

    Create Room model and execute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903063151) do
+ActiveRecord::Schema.define(version: 20180903063711) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "screen_name"
