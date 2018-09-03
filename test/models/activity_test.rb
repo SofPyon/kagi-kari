21473b165eb1030b8fb4c34c51c8f632d3bcd558
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 17:05:52 +0900

    Make activity_test cases

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @borrow_main = activities(:borrow_main)
+    @return_main = activities(:return_main)
+    @borrow_sub  = activities(:borrow_sub)
+  end
+
+  test "should be valid" do
+    assert @borrow_main.valid?
+    assert @return_main.valid?
+    assert @borrow_sub.valid?
+  end
+
+  test "action should be present" do
+    @borrow_main.action = nil
+    assert_not @borrow_main.valid?
+  end
+
+  test "action should be behaved as enum" do
+    assert @borrow_main.borrowing?
+    assert @return_main.returning?
+    assert @borrow_sub.borrowing?
+  end
 end
