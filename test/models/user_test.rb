670b0833b21fc1e8513d002889bce9162008ec43
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 16:12:15 +0900

    Add user_test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(screen_name: "hoge", name: "ほげ太郎", password: "hogehoge",
+                     password_confirmation: "hogehoge")
+  end
+
+  test "should valid" do
+    assert @user.valid?
+  end
+
+  test "screen_name should be present" do
+    @user.screen_name = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "　　"
+    assert_not @user.valid?
+  end
+
+  test "password should be present" do
+    @user.password = "   "
+    assert_not @user.valid?
+  end
+
+  test "password_confirmation should be same as password" do
+    @user.password_confirmation = "togetoge"
+    assert_not @user.valid?
+  end
+
+  test "screen_name should be at least 3 characters" do
+    @user.screen_name = "ho"
+    assert_not @user.valid?
+
+    @user.screen_name = "hog"
+    assert @user.valid?
+  end
+
+  test "screen_name should be at most 20 characters" do
+    @user.screen_name = "a" * 20
+    assert @user.valid?
+
+    @user.screen_name = "a" * 21
+    assert_not @user.valid?
+  end
+
+  test "name should be at most 20 characters" do
+    @user.screen_name = "あ" * 20
+    assert @user.valid?
+
+    @user.screen_name = "あ" * 21
+    assert_not @user.valid?
+  end
+
+  test "password should be at least 8 characters" do
+    @user.password = "a" * 7
+    assert_not @user.valid?
+
+    @user.password = "a" * 8
+    assert @user.valid?
+  end
 end
