cf0206214ca5191e551bf61b81c62f0e9e5672fb
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 22:17:35 +0900

    Make seeds for activities

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,12 @@ sofpyon = User.create(screen_name: "sofpyon", name: "SofPyon",
                       password: "password", password_confirmation: "password")
 
 # Rooms
-Room.create(name: "Main Room", description: "A room used mainly.")
-Room.create(name: "Sub Room", description: "")
+rooms = []
+rooms << Room.create(name: "Main Room", description: "A room used mainly.")
+rooms << Room.create(name: "Sub Room", description: "")
+
+# Activities
+rooms.each do |room|
+  room.activities.create(user_id: sofpyon.id, action: 1) # borrowing
+  room.activities.create(user_id: sofpyon.id, action: 0) # returning
+end
