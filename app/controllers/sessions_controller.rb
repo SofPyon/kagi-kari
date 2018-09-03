2b680d900a34aac0264bad4e6b7107b91b7deba8
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 18:42:13 +0900

    Redirect to log in form when ID/Password combination invalid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       # redirect_to root_path
     else
-      redirect_to new_session_path, alert: "Invalid ID/Password Combination."
+      flash[:danger] = "Invalid ID/Password Combination."
+      redirect_to new_session_path
     end
   end
 end
