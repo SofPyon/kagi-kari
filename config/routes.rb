ebf41e27b9d4113fb02c553ac7993133de517dfa
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 21:35:32 +0900

    Add rooms controller to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-
-  get 'rooms/new'
-
-  get 'rooms/edit'
-
-  get 'rooms/destroy'
-
   root 'static_pages#index'
   resources :sessions, only: [:new, :create]
   delete 'sessions', to: 'sessions#destroy'
+  resources :rooms
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
