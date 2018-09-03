e2944a4862b9d3761218739c43a90a964df48860
SofPyon <sofpyon@hrgrweb.com>
2018-09-03 15:43:12 +0900

    Set default value to Activity.action column

--- db/migrate/20180903064125_create_activities.rb
+++ db/migrate/20180903064125_create_activities.rb
@@ -3,7 +3,7 @@ class CreateActivities < ActiveRecord::Migration[5.1]
     create_table :activities do |t|
       t.reference :user
       t.reference :room
-      t.integer :action
+      t.integer :action, default: 1
 
       t.timestamps
     end
