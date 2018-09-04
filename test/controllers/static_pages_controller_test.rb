e5ec9135852af837bfcef4d6d751c825c1b5954b
SofPyon <sofpyon@hrgrweb.com>
2018-09-04 11:51:10 +0900

    Add test to get root with logged in

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
+  test "should get root with logged in" do
+    log_in_as(users(:john))
     get root_path
     assert_response :success
   end
