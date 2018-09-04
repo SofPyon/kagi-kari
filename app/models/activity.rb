b7f05deff9fe24fcf7bfbc0d59b60cf8de219f17
SofPyon <sofpyon@hrgrweb.com>
2018-09-04 18:38:01 +0900

    Add validation which checks if the action is appropriate.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,5 +3,18 @@ class Activity < ApplicationRecord
   belongs_to :room
   enum action: { borrowing: 1, returning: 0 }
   validates :action, presence: true
+  validate :repeated_action_forbidden
   default_scope { order(created_at: :desc) }
+
+  private
+
+    # custom validator
+    def repeated_action_forbidden
+      room.reload
+      if room.borrowed? && action == 'borrowing'
+        errors.add(:action, ": Already borrowed.")
+      elsif !room.borrowed? && action == 'returning'
+        errors.add(:action, ": Already returned.")
+      end
+    end
 end
