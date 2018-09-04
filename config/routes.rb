f8cae7e85f5b9ecc3d685bf6754e7388e85ceb08
SofPyon <sofpyon@hrgrweb.com>
2018-09-04 12:30:42 +0900

    Add route for activities

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create]
   delete 'sessions', to: 'sessions#destroy'
   resources :rooms
+  resources :activities, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
