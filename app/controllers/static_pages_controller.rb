aa4022d99f19cd0a9d129ffb0603c16cfd69db3f
SofPyon <sofpyon@hrgrweb.com>
2018-09-04 18:45:53 +0900

    At root page, make @rooms to include activities collection with user.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :require_log_in
 
   def index
-    @rooms = Room.all
+    @rooms = Room.includes(activities: :user).all
     @activities = Activity.all
   end
 end
